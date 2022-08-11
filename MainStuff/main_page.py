eade7783821b950baab930a0a371cc2099543005
Kenshin <zomaliense01@gmail.com>
2022-08-11 19:42:13 -0400

    prueba de usuaros bloqueados finalizada.

--- MainStuff/main_page.py
+++ MainStuff/main_page.py
@@ -1,9 +1,9 @@
 import time
-
 from selenium.webdriver.support import expected_conditions as EC
 from MainStuff.base import Base
-
 from selenium.webdriver.common.by import By
+
+
 class Mainpage(Base):
     # metodo constructor cuando creamos objeto de esta clase va inicializar con los siguiente atri
     def __init__(self,driver,wait):
@@ -14,11 +14,11 @@ class Mainpage(Base):
     def ir_pagina_busc(self):
         self.ir_pagina(self.url) # aqui estoy iendo a la pagina que le pasamos
 
-
     def chek_url(self,url):
         assert self.give_url() == url # si la url que le pasamos es igual a la url true si no tira error..
 
 
+    # este metodo se encargara de acceder a los elementos para hacer la prueba del login fallando...
     def login_fail(self,username,password): # metodo para hacer prueba de fallo con el login
 
         self.driver.find_element(By.XPATH,"//*[@id='user-name']").send_keys(username) # USERNAME FIELD
@@ -28,7 +28,7 @@ class Mainpage(Base):
 
         self.driver.save_screenshot('C:/Users/zomal/PycharmProjects/pyTestSwagLabs/results/fail_login.png') #SC FAIL LOGIN
 
-
+    # este metodo se encargara de acceder a los elementos para hacer la prueba del login exitosa...
     def log_succes(self,username,password): # metodo para hacer prueba del login correctamente
 
         self.driver.find_element(By.XPATH,"//*[@id='user-name']").send_keys(username) #USERNAME
@@ -41,7 +41,7 @@ class Mainpage(Base):
         # screenshot cuando es exitoso
         self.driver.save_screenshot('C:/Users/zomal/PycharmProjects/pyTestSwagLabs/results/Login_succes.png') #SC LOGIN SUCCES
 
-
+    # este metodo se encargara de acceder a los elementos para hacer la prueba del checkout fallida...
     def checkout_fail(self, username,password):  # metodo para hacer prueba del login correctamente
 
         #logeando
@@ -69,7 +69,7 @@ class Mainpage(Base):
         self.driver.save_screenshot(
             'C:/Users/zomal/PycharmProjects/pyTestSwagLabs/results/checkout_fail.png')
 
-
+    # este metodo se encargara de acceder a los elementos para hacer la prueba del checkout exitosa...
     def checkout_succes(self, username,password,first,last,zip):  # metodo para hacer prueba del login correctamente
 
         #logeando
@@ -108,7 +108,7 @@ class Mainpage(Base):
         self.driver.save_screenshot(
             'C:/Users/zomal/PycharmProjects/pyTestSwagLabs/results/checkout_succes.png')
 
-
+    # este metodo se encarga de probar que los productos cargan exitosamente....
     def products_succes(self,username,password):
 
         # logeando
@@ -121,12 +121,38 @@ class Mainpage(Base):
         self.driver.save_screenshot(
             'C:/Users/zomal/PycharmProjects/pyTestSwagLabs/results/products_succes.png')
 
+    # este metodo se encarga de probar que los productos al cargar se muestran repetidas....
     def products_fail(self, username, password):
         # logeando
         self.driver.find_element(By.XPATH, "//*[@id='user-name']").send_keys(username)  # USERNAME
         self.driver.find_element(By.XPATH, "//*[@id='password']").send_keys(password)  # PASSWORD
         self.driver.find_element(By.XPATH, "//*[@id='login-button']").click()  # CLICK LOGIN
         time.sleep(3)
-        # tirando sc cuando es exitoso
+        # tirando sc cuando es fallido
         self.driver.save_screenshot(
             'C:/Users/zomal/PycharmProjects/pyTestSwagLabs/results/products_fail.png')
+
+    # este metodo se encarga de probar que el usuario no esta bloqueado
+    def user_notblock(self,username,password):
+
+        # logeando
+        self.driver.find_element(By.XPATH, "//*[@id='user-name']").send_keys(username)  # USERNAME
+        self.driver.find_element(By.XPATH, "//*[@id='password']").send_keys(password)  # PASSWORD
+        self.driver.find_element(By.XPATH, "//*[@id='login-button']").click()  # CLICK LOGIN
+
+        time.sleep(3)
+        # tirando sc cuando es exitoso
+        self.driver.save_screenshot(
+            'C:/Users/zomal/PycharmProjects/pyTestSwagLabs/results/usernotblock_succes.png')
+
+    # este metodo se encarga de probar que el usuario esta bloqueado
+    def user_block(self, username, password):
+        # logeando
+        self.driver.find_element(By.XPATH, "//*[@id='user-name']").send_keys(username)  # USERNAME
+        self.driver.find_element(By.XPATH, "//*[@id='password']").send_keys(password)  # PASSWORD
+        self.driver.find_element(By.XPATH, "//*[@id='login-button']").click()  # CLICK LOGIN
+
+        time.sleep(3)
+        # tirando sc cuando es exitoso
+        self.driver.save_screenshot(
+            'C:/Users/zomal/PycharmProjects/pyTestSwagLabs/results/userblock_succes.png')
--- SeleTest/test_Final.py
+++ SeleTest/test_Final.py
@@ -6,27 +6,34 @@ from selenium.webdriver.support.ui import WebDriverWait
 from MainStuff.main_page import Mainpage
 
 class TestSearh():
+
     @pytest.fixture
     def Test_inicialP(self):
-        # options to add as arguments
-        from selenium.webdriver.chrome.options import Options
+
+        #******************* configuraciones de selenium *******************
+
         option = webdriver.ChromeOptions()
         option.add_argument("start-maximized")
 
-        # chrome to stay open
-        option.add_experimental_option("detach", True)
+
+        option.add_experimental_option("detach", True) # chrome to stay open
 
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
         self.wait = WebDriverWait(self.driver,10)
 
-
-
+        # ****************************************************************************************************
 
         self.page = Mainpage(self.driver,self.wait)
         self.page.ir_pagina_busc()
 
+    #################### TESTS ######################
+
+    ###### puebas url ##############################
+
     def test_url(self,Test_inicialP):
         self.page.chek_url("https://youtube.com")
+
+    #################### Login tests ########################
 
     def test_login_fail(self,Test_inicialP):
         self.page.login_fail('asdasdasd','asdasdasdasd')
@@ -34,13 +41,26 @@ class TestSearh():
     def test_login_succes(self,Test_inicialP):
         self.page.log_succes('standard_user','secret_sauce')
 
+    ###################### checkout products tests ###########
+
     def test_checkout_fail(self,Test_inicialP):
         self.page.checkout_fail('standard_user','secret_sauce')
 
     def test_checkout_succes(self,Test_inicialP):
         self.page.checkout_succes('standard_user','secret_sauce','jonathan','montero','12345')
 
+##################### products test###########################
+
     def test_product_succes(self,Test_inicialP):
         self.page.products_succes('standard_user','secret_sauce')
+
     def test_product_fail(self,Test_inicialP):
         self.page.products_fail('problem_user','secret_sauce')
+
+################### block users tests #########################
+
+    def test_user_not_block(self,Test_inicialP):
+        self.page.user_notblock('standard_user','secret_sauce')
+
+    def test_user_block(self,Test_inicialP):
+        self.page.user_block('locked_out_user','secret_sauce')
